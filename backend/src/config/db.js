065005442cffa7f76701243164c7c38c809ff075
Himanshu Kumar (Yadav) <hiyadav2022@gmail.com>
2026-09-20 17:24:41 +0530

    Add backend configuration and URL model for URL shortener service
    - Create .gitignore to exclude unnecessary files
    - Update .env for local MongoDB connection
    - Implement config.js to manage environment variables
    - Modify db.js to connect to MongoDB using config
    - Add url.model.js for URL schema definition
    - Update server.js to import app and establish database connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-
+import config from "./config.js";
 
 export async function connectDB() {
 
-    await mongoose.connect()
+    await mongoose.connect(config.MONGO_URI)
     console.log("Connected to MongoDB");
 
     
